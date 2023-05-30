334f2dcabcf2a2bc7dfaf17017f7e274082f17c2
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-30 08:14:39 +0300

    feat: add task nested components

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./site/Header";
+import {Body} from "./site/Body";
+import {Footer} from "./site/Footer";
+
+function App() {
+    return (
+        <div className="App">
+            <Header title={'New Header'}/>
+            <Body titleForBody={'New Body'}/>
+            <Footer titleForFooter={'New Footer'}/>
+        </div>
+    );
+}
+
+export default App;
