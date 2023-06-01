aea4bb0178454892536c22b256454ffca4c1e491
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-01 21:47:48 +0300

    feat: add task method filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,55 @@
 import React, {useState} from 'react';
 import './App.css';
 
+type FilterName = "all" | "ruble" | "dollar";
 
 function App() {
+    const [money, setMoney] = useState([
+        {banknote: "dollar", nominal: 100, number: "a123456789"},
+        {banknote: "dollar", nominal: 50, number: "b123456789"},
+        {banknote: "ruble", nominal: 100, number: "c123456789"},
+        {banknote: "dollar", nominal: 100, number: "d123456789"},
+        {banknote: "dollar", nominal: 50, number: "e123456789"},
+        {banknote: "ruble", nominal: 100, number: "f123456789"},
+        {banknote: "dollar", nominal: 50, number: "j123456789"},
+        {banknote: "ruble", nominal: 50, number: "h123456789"},
+        {banknote: "ruble", nominal: 50, number: "h123456789"},
+        {banknote: "ruble", nominal: 50, number: "h123456789"},
+    ])
 
-    /* let students = [
-         {id: 1, name: "James", age: 8},
-         {id: 2, name: "Robert", age: 18},
-         {id: 3, name: "John", age: 28},
-         {id: 4, name: "Michael", age: 38},
-         {id: 5, name: "William", age: 48},
-         {id: 6, name: "David", age: 58},
-         {id: 7, name: "Richard", age: 68},
-         {id: 8, name: "Joseph", age: 78},
-         {id: 9, name: "Thomas", age: 88},
-         {id: 10, name: "Charles", age: 98},
-         {id: 11, name: "Christopher", age: 100},
-     ]
-     const topCars = [
-         {manufacturer: 'BMW', model: 'm5cs'},
-         {manufacturer: 'Mercedes', model: 'e63s'},
-         {manufacturer: 'Audi', model: 'rs6'},
-         {manufacturer: 'Mercedes', model: 'e63s'},
-         {manufacturer: 'Mercedes', model: 'e63s'},
-         {manufacturer: 'Mercedes', model: 'e63s'},
+    const [filter, setFilter] = useState<FilterName>('all');
 
-     ];*/
+    let currentMoney = money;
 
-    /*const myFirstSubscriberFirst = () => {*/
-    /*    console.log("Hello Vasya");*/
-    /*}*/
-    /*const myFirstSubscriberSecond = () => {*/
-    /*    console.log("Hello Ivan");*/
-    /*}*/
-
-    const Button1Foo = (name: string, age: number) => {
-        console.log(`I'm ${name}. My age : ${age}`);
+    if (filter === 'ruble') {
+        currentMoney = money.filter((filteredMoney: { banknote: string, nominal: number, number: string }) => filteredMoney.banknote === "ruble")
     }
-    const Button2Foo = (name: string) => {
-        console.log(`I'm ${name}`);
-
-    }
-    const Button3Foo = () => {
-        console.log("I'm stupping button");
+    if (filter === 'dollar') {
+        currentMoney = money.filter((filteredMoney: { banknote: string, nominal: number, number: string }) => filteredMoney.banknote === "dollar")
     }
 
-    let [a, setA] = useState(1)
-    const onClickHand = () => {
-        setA(++a);
-        console.log(a);
-    }
-    const onClickHandReset = () => {
-        setA(0)
-    }
+    const onClickFilterHandler = (nameButton: FilterName) => {
+        setFilter(nameButton)
+    };
+
 
     return (
         <div className="App">
-            {/* <Header title={'New Header'}/>
-            <Body titleForBody={'New Body'}/>
-            <Footer titleForFooter={'New Footer'}/>
-            <NewComponent students={students}/>
-            <ComponentWork topcars={topCars}/>*/}
-
-            {/* <Button name={'MyYouTubeChannel-1'} callBack={() => Button1Foo('Stas', 38)}/>
-            <Button name={'MyYouTubeChannel-2'} callBack={() => Button2Foo('Ivan')}/>
-            <Button name={'Stypid button'} callBack={Button3Foo}/>*/}
-
-
-            <h1>{a}</h1>
-            <button onClick={onClickHand}>Number</button>
-            <button onClick={onClickHandReset}>Reset</button>
-
-
+            <ul>
+                {currentMoney.map((el, index) => {
+                    return (
+                        <li key={index}><span>{el.banknote}</span>
+                            <span> {el.nominal}</span>
+                            <span> {el.number}</span>
+                        </li>
+                    );
+                })}
+            </ul>
+            <div className="buttons">
+                <button onClick={() => onClickFilterHandler('all')}>all</button>
+                <button onClick={() => onClickFilterHandler('ruble')}>ruble</button>
+                <button onClick={() => onClickFilterHandler('dollar')}>dollar</button>
+            </div>
         </div>
     );
 }
